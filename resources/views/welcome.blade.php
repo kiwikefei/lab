550e0d73cd3540460b60a93fe26ca559ab88f396
Andy Tang <kiwikefei@gmail.com>
2017-10-02 09:11:30 +1300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Lab</title>
+        <style>
+            html, body{
+                height: 100%;
+            }
+            body {
+                font-size: 16px;
+                font-family: sans-serif;
+                margin:0;
+                display: flex;
+                align-items: center;
+                justify-content: center ;
+            }
+            nav {
+                background: #eeeeee;
+                width: 100%;
+            }
+            nav > ul {
+                margin: 0;
+                padding: 0;
+                display: flex;
+                justify-content: space-around;
+                list-style: none;
+            }
+            .nav-item {
+                padding: 3em;
+            }
+        </style>
+    </head>
+    <body>
+
+    <nav>
+        <ul>
+            <li class="nav-item"><a href="#">Home</a></li>
+            <li class="nav-item"><a href="#">Blog</a></li>
+            <li class="nav-item"><a href="#">Contact</a></li>
+            <li class="nav-item"><a href="#">About</a></li>
+        </ul>
+    </nav> 
+    </body>
+</html>
