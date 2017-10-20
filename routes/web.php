8f86131aba21b3748244ff0b04fe7cc5e7af42b2
Andy Tang <kiwikefei@gmail.com>
2017-10-20 14:35:00 +1300

    laravel 5.5.18

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,16 @@ Route::post('notes', 'NoteController@store');
 Route::get('notes', 'NoteController@index');
 Route::get('notes/create', 'NoteController@create');
 Route::get('tests', 'TestController@index');
+class Test 
+{
+    public $name;
+}
+Route::get('', function(){
+    $test = new Test;
+    $test->name = 'Andy';
+    $test1 = $test;
+    $test2 = clone $test;
+    $test1->name = 'Duck';
+    $test2->name = 'Hello';
+    dd($test,$test1,$test2);
+});
